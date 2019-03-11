9c7b636f4f7d80d2e940e056471ad6a1d28969d8
thanh4890 <thanh4890@gmail.com>
2019-03-12 00:15:33 +0700

    Draft for jwt

--- app.js
+++ app.js
@@ -31,7 +31,17 @@ passport.use(
       usernameField: 'email',
       passwordField: 'passwd'
     },
-    User.authenticate()
+    function(email, passwd, cb) {
+      //this one is typically a DB call. Assume that the returned user object is pre-formatted and ready for storing in JWT
+      return User.findOne({ email, passwd })
+        .then(user => {
+          if (!user) {
+            return cb(null, false, { message: 'Incorrect email or password.' })
+          }
+          return cb(null, user, { message: 'Logged In Successfully' })
+        })
+        .catch(err => cb(err))
+    }
   )
 )
 
--- models/user.js
+++ models/user.js
@@ -3,10 +3,8 @@ var Schema = mongoose.Schema
 var passportLocalMongoose = require('passport-local-mongoose')
 
 var UserSchema = new Schema({
-  username: String,
-  password: String
+  email: String,
+  passwd: String
 })
-
-UserSchema.plugin(passportLocalMongoose)
 
 module.exports = mongoose.model('User', UserSchema)
