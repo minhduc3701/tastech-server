e4fcbb56b7e39f444e0f4daacef92cfd616f9fe0
thanh4890 <thanh4890@gmail.com>
2019-03-11 17:36:47 +0700

    register, login for /users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var passport = require('passport')
+var User = require('../models/user')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+  res.send('respond with a resource')
+})
 
-module.exports = router;
+router.post('/', function(req, res) {
+  User.register(
+    new User({
+      username: req.body.email
+    }),
+    req.body.passwd,
+    function(err, user) {
+      if (err) {
+        res.status(400).send(err)
+      }
+
+      res.status(200).send(user)
+    }
+  )
+})
+
+router.post(
+  '/login',
+  passport.authenticate('local', { session: false }),
+  function(req, res) {
+    res.status(200).send(req.user)
+  }
+)
+
+module.exports = router
