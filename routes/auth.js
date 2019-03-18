0ff1ae60baed4228941de3edfe020babc599c8a8
thanh4890 <thanh4890@gmail.com>
2019-03-18 21:47:20 +0700

    Config for dev test env

--- routes/auth.js
+++ routes/auth.js
@@ -46,7 +46,7 @@ router.post('/login', function(req, res, next) {
       // generate a signed son web token with the contents of user object and return it in the response
       const token = jwt.sign(
         { id: user.id, email: user.username },
-        'ILovePokemon'
+        process.env.JWT_SECRET
       )
       return res.json({ user: user.username, token })
     })
