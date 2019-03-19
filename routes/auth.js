a711cca743b4eff3c33e4b861983ac35e3838468
thanh4890 <thanh4890@gmail.com>
2019-03-19 18:03:14 +0700

    Send mail with sendgrid for forgot-password

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,9 @@ var User = require('../models/user')
 var jwt = require('jsonwebtoken')
 const { ObjectID } = require('mongodb')
 const crypto = require('crypto')
+const async = require('async')
+const nodemailer = require('nodemailer')
+const sgTransport = require('nodemailer-sendgrid-transport')
 
 router.post('/register', function(req, res) {
   User.register(
@@ -57,34 +60,77 @@ router.post('/login', function(req, res, next) {
 })
 
 router.post('/forgot-password', function(req, res) {
-  crypto.randomBytes(20, function(err, buf) {
-    var token = buf.toString('hex')
+  async.waterfall(
+    [
+      function(done) {
+        crypto.randomBytes(20, function(err, buf) {
+          var token = buf.toString('hex')
+          done(err, token)
+        })
+      },
+      function(token, done) {
+        User.findOneAndUpdate(
+          {
+            email: req.body.email
+          },
+          {
+            $set: {
+              resetPasswordToken: token,
+              resetPasswordExpires: Date.now() + 3600000 // 1 hour
+            }
+          },
+          {
+            new: true
+          }
+        )
+          .then(user => {
+            if (!user) {
+              return res.status(404).send()
+            }
 
-    User.findOneAndUpdate(
-      {
-        email: req.body.email
+            done(null, token, user)
+          })
+          .catch(e => {
+            done(e)
+          })
       },
-      {
-        $set: {
-          resetPasswordToken: token,
-          resetPasswordExpires: Date.now() + 3600000 // 1 hour
+      function(token, user, done) {
+        var options = {
+          auth: {
+            api_user: process.env.SENDGRID_USERNAME,
+            api_key: process.env.SENDGRID_PASSWORD
+          }
         }
-      },
-      {
-        new: true
+
+        var client = nodemailer.createTransport(sgTransport(options))
+        var mailOptions = {
+          to: user.email,
+          from: 'no-reply@eztrip.com',
+          subject: `Password Reset for ${user.email}`,
+          text:
+            'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+            'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+            'http://' +
+            req.headers.host +
+            '/reset-password/' +
+            token +
+            '\n\n' +
+            'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+        }
+
+        client.sendMail(mailOptions, function(err, info) {
+          done(err, user)
+        })
       }
-    )
-      .then(user => {
-        if (!user) {
-          return res.status(404).send()
-        }
+    ],
+    function(err, user) {
+      if (err) {
+        return res.status(400).send(err)
+      }
 
-        res.status(200).send({ token })
-      })
-      .catch(e => {
-        res.status(400).send(e)
-      })
-  })
+      res.status(200).send({ user })
+    }
+  )
 })
 
 router.post('/reset-password/:token', function(req, res) {
