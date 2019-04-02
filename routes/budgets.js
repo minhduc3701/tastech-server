e5995076c16f218c576f216587e1535a9604968a
thanh4890 <thanh4890@gmail.com>
2019-04-02 14:13:16 +0700

    Fix bug routes/budgets.js file name

--- routes/budgets.js
+++ routes/budgets.js
@@ -4,15 +4,15 @@ var Budget = require('../models/budget')
 
 router.post('/', function(req, res, next) {
   const request = new Budget(req.body)
-  request._creator = req.user._id,
-  request
-    .save()
-    .then(() => {
-      res.status(200).json(Budget)
-    })
-    .catch(e => {
-      res.status(400).send()
-    })
+  ;(request._creator = req.user._id),
+    request
+      .save()
+      .then(() => {
+        res.status(200).json(Budget)
+      })
+      .catch(e => {
+        res.status(400).send()
+      })
 })
 
 module.exports = router
