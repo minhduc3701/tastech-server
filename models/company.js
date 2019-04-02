150908584e45a338d5c744b6f9a6429c139bb10f
thanh4890 <thanh4890@gmail.com>
2019-04-02 14:00:36 +0700

    Update _company, _admin, _department for User, change _owner to _admin for Company

--- models/company.js
+++ models/company.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const CompanySchema = new Schema({
   name: String,
-  _owner: mongoose.Schema.Types.ObjectId,
+  _admin: mongoose.Schema.Types.ObjectId,
   _creator: mongoose.Schema.Types.ObjectId,
   departments: Array
 })
