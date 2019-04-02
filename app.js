eafb4beabae74101f9186fba1030a296d7cc2248
thanh4890 <thanh4890@gmail.com>
2019-04-02 12:12:59 +0700

    Add /tas-admin/companies resources

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500).send({ error: 'Not Found' })
+  res.status(err.status || 500).send()
 })
 
 module.exports = app
