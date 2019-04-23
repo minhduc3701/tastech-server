284097f882be9ec82d5f566f6d13665fc62d93da
thanh4890 <thanh4890@gmail.com>
2019-04-23 09:24:41 +0700

    Add ticket model

--- routes/flights/pkfare.js
+++ routes/flights/pkfare.js
@@ -1,33 +1,26 @@
 const express = require('express')
 const router = express.Router()
 const _ = require('lodash')
+const Ticket = require('../../models/ticket')
 
 // @see http://open.pkfare.com/documents/show?id=2352d3737b0442d6a402fea86ed8bda2uk
 router.post('/', (req, res) => {
-  let body = _.pick(req.body, [
-    'orderNum',
-    'airPnr',
-    'paymentGate',
-    'serialNum',
-    'merchantOrder',
-    'permitVoid',
-    'lastVoidTime',
-    'voidServiceFee',
-    'currency',
-    'ticketNums'
-  ])
+  let ticket = new Ticket(req.body)
 
-  if (!body.orderNum) {
-    return res.status(400).send({
-      errorCode: 1,
-      errorMsg: 'Failure'
+  ticket
+    .save()
+    .then(() => {
+      res.status(200).send({
+        errorCode: 0,
+        errorMsg: 'ok'
+      })
     })
-  }
-
-  res.status(200).send({
-    errorCode: 0,
-    errorMsg: 'ok'
-  })
+    .catch(e => {
+      res.status(400).send({
+        errorCode: 1,
+        errorMsg: 'Failure'
+      })
+    })
 })
 
 module.exports = router
