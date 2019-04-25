f55a195890979c14bdbe5df8828d51891e7b0b5c
thanh4890 <thanh4890@gmail.com>
2019-04-25 15:52:29 +0700

    Update pkfare endpoints

--- routes/flights/pkfare.js
+++ routes/flights/pkfare.js
@@ -25,32 +25,20 @@ router.post('/', (req, res) => {
     })
 })
 
+const authentication = {
+  partnerId: process.env.PKFARE_PARTNER_ID,
+  sign: process.env.PKFARE_SIGN
+}
+
 router.post('/shoppingV2', (req, res) => {
   let base64 = Buffer.from(
     JSON.stringify({
-      search: {
-        adults: 1,
-        children: 0,
-        infants: 0,
-        nonstop: 0,
-        searchAirLegs: [
-          {
-            cabinClass: 'Economy',
-            departureDate: '2019-11-07',
-            destination: 'MEL',
-            origin: 'HAN'
-          }
-        ],
-        solutions: 0
-      },
-      authentication: {
-        partnerId: 'P3kScN47kwu1+Rcj4JlgNdXqCzQ=',
-        sign: 'f314b98e80370644fd11a95453930d2f'
-      }
+      search: req.body.search,
+      authentication
     })
   ).toString('base64')
   request(
-    'https://open.pkfare.com/apitest/shoppingV2?param=' + base64,
+    `${process.env.PKFARE_URI}/shoppingV2?param=${base64}`,
     { encoding: null },
     function(err, response, body) {
       zlib.gunzip(body, function(err, dezipped) {
@@ -63,132 +51,91 @@ router.post('/shoppingV2', (req, res) => {
 router.post('/precisePricing', (req, res) => {
   let base64 = Buffer.from(
     JSON.stringify({
-      pricing: {
-        adults: 1,
-        children: 0,
-        infants: 0,
-        journeys: {
-          journey_0: [
-            {
-              airline: 'MH',
-              arrival: 'SYD',
-              arrivalDate: '2019-11-08',
-              arrivalTime: '10:55',
-              bookingCode: 'Y',
-              departure: 'KUL',
-              departureDate: '2019-11-07',
-              departureTime: '23:35',
-              flightNum: '123'
-            }
-          ]
-        }
-      },
-      authentication: {
-        partnerId: 'P3kScN47kwu1+Rcj4JlgNdXqCzQ=',
-        sign: 'f314b98e80370644fd11a95453930d2f'
-      }
+      pricing: req.body.pricing,
+      authentication
     })
   ).toString('base64')
-  request(
-    'https://open.pkfare.com/apitest/precisePricing?param=' + base64,
-    function(err, response, body) {
-      res.send(JSON.parse(body))
-    }
-  )
+  request(`${process.env.PKFARE_URI}/precisePricing?param=${base64}`, function(
+    err,
+    response,
+    body
+  ) {
+    res.send(JSON.parse(body))
+  })
+})
+
+router.post('/penalty', (req, res) => {
+  let base64 = Buffer.from(
+    JSON.stringify({
+      penalty: req.body.penalty,
+      authentication
+    })
+  ).toString('base64')
+  request(`${process.env.PKFARE_URI}/penalty?param=${base64}`, function(
+    err,
+    response,
+    body
+  ) {
+    res.send(JSON.parse(body))
+  })
 })
 
 router.post('/preciseBooking', (req, res) => {
   let base64 = Buffer.from(
     JSON.stringify({
-      booking: {
-        passengers: [
-          {
-            birthday: '1990-04-25',
-            cardExpiredDate: '2022-04-14',
-            cardNum: '1234',
-            cardType: 'P',
-            firstName: 'Giang',
-            lastName: 'Do',
-            nationality: 'VN',
-            psgType: 'ADT',
-            sex: 'M'
-          }
-        ],
-        solution: {
-          adtFare: 0,
-          adtTax: 0,
-          infFare: 0,
-          infTax: 0,
-          journeys: {
-            journey_0: [
-              {
-                airline: 'MH',
-                arrival: 'SYD',
-                arrivalDate: '2019-11-08',
-                arrivalTime: '10:55',
-                bookingCode: 'Y',
-                departure: 'KUL',
-                departureDate: '2019-11-07',
-                departureTime: '23:35',
-                flightNum: '123'
-              }
-            ]
-          }
-        }
-      },
-      authentication: {
-        partnerId: 'P3kScN47kwu1+Rcj4JlgNdXqCzQ=',
-        sign: 'f314b98e80370644fd11a95453930d2f'
-      }
+      booking: req.body.booking,
+      authentication
     })
   ).toString('base64')
-  request(
-    'https://open.pkfare.com/apitest/preciseBooking?param=' + base64,
-    function(err, response, body) {
-      res.send(JSON.parse(body))
-    }
-  )
+  request(`${process.env.PKFARE_URI}/preciseBooking?param=${base64}`, function(
+    err,
+    response,
+    body
+  ) {
+    res.send(JSON.parse(body))
+  })
+})
+
+router.post('/cancel', (req, res) => {
+  let base64 = Buffer.from(
+    JSON.stringify({
+      cancel: req.body.cancel,
+      authentication
+    })
+  ).toString('base64')
+  request(`${process.env.PKFARE_URI}/cancel?param=${base64}`, function(
+    err,
+    response,
+    body
+  ) {
+    res.send(JSON.parse(body))
+  })
 })
 
 router.post('/orderPricing', (req, res) => {
   let base64 = Buffer.from(
     JSON.stringify({
-      orderPricing: {
-        indicatorBIC: 0,
-        isReBook: 0,
-        orderNum: 1025001773
-      },
-      authentication: {
-        partnerId: 'P3kScN47kwu1+Rcj4JlgNdXqCzQ=',
-        sign: 'f314b98e80370644fd11a95453930d2f'
-      }
+      orderPricing: req.body.orderPricing,
+      authentication
     })
   ).toString('base64')
-  request(
-    'https://open.pkfare.com/apitest/orderPricing?param=' + base64,
-    function(err, response, body) {
-      res.send(JSON.parse(body))
-    }
-  )
+  request(`${process.env.PKFARE_URI}/orderPricing?param=${base64}`, function(
+    err,
+    response,
+    body
+  ) {
+    res.send(JSON.parse(body))
+  })
 })
 
 router.post('/ticketing', (req, res) => {
   let base64 = Buffer.from(
     JSON.stringify({
-      ticketing: {
-        email: 'giang.do@tastech.asia',
-        name: 'Giang',
-        orderNum: 1025001773,
-        pnr: 'UBV8OI',
-        telNum: '12345678'
-      },
-      authentication: {
-        partnerId: 'P3kScN47kwu1+Rcj4JlgNdXqCzQ=',
-        sign: 'f314b98e80370644fd11a95453930d2f'
-      }
+      ticketing: req.body.ticketing,
+      authentication
     })
   ).toString('base64')
-  request('https://open.pkfare.com/apitest/ticketing?param=' + base64, function(
+  request(`${process.env.PKFARE_URI}/ticketing?param=${base64}`, function(
     err,
     response,
     body
