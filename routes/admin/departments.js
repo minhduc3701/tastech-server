3d4b5a9c02a6a8ae1cabbc0307b956671c42a2ba
Giang Do <ngocgiang2512@gmail.com>
2019-04-26 10:02:56 +0700

    ez68 create department api

--- routes/admin/departments.js
+++ routes/admin/departments.js
@@ -3,6 +3,19 @@ const router = express.Router()
 const Department = require('../../models/department')
 const { ObjectID } = require('mongodb')
 
+router.post('/', function(req, res, next) {
+  const department = new Department(req.body)
+  department._company = req.user._company
+  department
+    .save()
+    .then(() => {
+      res.status(200).json({ department })
+    })
+    .catch(e => {
+      res.status(400).send()
+    })
+})
+
 router.get('/', (req, res) => {
   Department.find({ _company: req.user._company })
     .then(departments => res.status(200).send({ departments }))
