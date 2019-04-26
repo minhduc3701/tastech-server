1d33c43afc3fb8ab14bc91e000e7a0e7b2a03c8a
thanh4890 <thanh4890@gmail.com>
2019-04-26 10:50:33 +0700

    Hot fix pkfare, must allow text as content-type for POST /flights/pkfare

--- routes/flights/pkfare.js
+++ routes/flights/pkfare.js
@@ -2,10 +2,23 @@ const express = require('express')
 const router = express.Router()
 const _ = require('lodash')
 const Ticket = require('../../models/ticket')
+const bodyParser = require('body-parser')
 
 // @see http://open.pkfare.com/documents/show?id=2352d3737b0442d6a402fea86ed8bda2uk
-router.post('/', (req, res) => {
-  let ticket = new Ticket(req.body)
+// @see https://stackoverflow.com/a/30099608
+router.post('/', bodyParser.text({ type: '*/*' }), (req, res) => {
+  let body
+
+  try {
+    body = JSON.parse(req.body)
+  } catch (e) {
+    return res.status(400).send({
+      errorCode: 1,
+      errorMsg: 'Failure'
+    })
+  }
+
+  let ticket = new Ticket(body)
 
   ticket
     .save()
