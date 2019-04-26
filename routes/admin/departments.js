e15c7ad481d35009b0e21d7c3df3ad9b17ae7457
Giang Do <ngocgiang2512@gmail.com>
2019-04-26 10:09:45 +0700

    ez69 get department api

--- routes/admin/departments.js
+++ routes/admin/departments.js
@@ -22,4 +22,25 @@ router.get('/', (req, res) => {
     .catch(e => res.status(400).send())
 })
 
+router.get('/:id', function(req, res) {
+  if (!ObjectID.isValid(req.params.id)) {
+    return res.status(404).send()
+  }
+
+  Department.findOne({
+    _id: req.params.id,
+    _company: req.user._company
+  })
+    .then(department => {
+      if (!department) {
+        return res.status(404).send()
+      }
+
+      res.status(200).send({ department })
+    })
+    .catch(e => {
+      res.status(400).send()
+    })
+})
+
 module.exports = router
