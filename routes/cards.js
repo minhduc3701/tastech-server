5a635fa2fd37ab55726d48c3c02ff24a44fd2632
thanh4890 <thanh4890@gmail.com>
2019-05-15 15:15:27 +0700

    Calculate price to charge

--- routes/cards.js
+++ routes/cards.js
@@ -5,6 +5,10 @@ const Card = require('../models/card')
 router.post('/', function(req, res, next) {
   const token = req.body.token
 
+  if (!token) {
+    return res.status(400).send()
+  }
+
   // Set your secret key: remember to change this to your live secret key in production
   // See your keys here: https://dashboard.stripe.com/account/apikeys
   const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
--- routes/checkout.js
+++ routes/checkout.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const Card = require('../models/card')
+const Trip = require('../models/trip')
 
 router.post('/card', function(req, res, next) {
-  const cardId = req.body.card.id
+  const { card, trip } = req.body
+  let cardId = card.id
+  let tripId = trip.id
 
   // Set your secret key: remember to change this to your live secret key in production
   // See your keys here: https://dashboard.stripe.com/account/apikeys
@@ -11,24 +14,48 @@ router.post('/card', function(req, res, next) {
 
   ;(async () => {
     try {
-      let card = await Card.findOne({
+      // calculate the trip price here
+      let foundTrip = await Trip.findById(tripId)
+
+      if (!foundTrip) {
+        return res.status(400).send()
+      }
+
+      let adultPriceBreakdown = ['adtFare', 'adtTax', 'tktFee']
+
+      let serviceFeeBreadkdown = ['platformServiceFee', 'merchantFee']
+
+      let adultPrice = adultPriceBreakdown.reduce(
+        (acc, fee) => foundTrip.flight[fee] + acc,
+        0
+      )
+      adultPrice *= foundTrip.passengers.length
+      let serviceFee = serviceFeeBreadkdown.reduce(
+        (acc, fee) => foundTrip.flight[fee] + acc,
+        0
+      )
+
+      const amount = adultPrice + serviceFee
+
+      // find the card
+      let foundCard = await Card.findOne({
         _id: cardId,
         owner: req.user._id
       })
 
-      if (!card) {
+      if (!foundCard) {
         return res.status(400).send()
       }
 
       // When it's time to charge the customer again, retrieve the customer ID.
       const charge = await stripe.charges.create({
-        amount: 1500, // $15.00 this time
+        amount: amount * 100,
         currency: 'usd',
-        customer: card.customer.id // Previously stored, then retrieved
+        customer: foundCard.customer.id // Previously stored, then retrieved
       })
 
       // YOUR CODE: Save the customer ID and other info in a database for later.
-      res.status(200).send({ charge })
+      res.status(200).send({ status: charge.status })
     } catch (e) {
       res.status(400).send()
     }
