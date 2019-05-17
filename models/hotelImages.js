71bc1eb60e4558d76286101797210238a54eb76a
Giang Do <ngocgiang2512@gmail.com>
2019-05-17 10:44:02 +0700

    ez306 update api for hotel descriptions, amenities, policies and transportations

--- models/hotelImages.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const HotelImageSchema = new Schema({
-  _id: Number
-})
-
-module.exports = mongoose.model('HotelImage', HotelImageSchema)
--- routes/hotels/pkfare.js
+++ routes/hotels/pkfare.js
@@ -4,19 +4,12 @@ const _ = require('lodash')
 const passport = require('passport')
 const axios = require('axios')
 const Hotel = require('../../models/hotel')
-const HotelImage = require('../../models/hotelImages')
 const { ObjectID } = require('mongodb')
 
 const authentication = {
   partnerId: process.env.PKFARE_PARTNER_ID,
   sign: process.env.PKFARE_SIGN
 }
-
-router.get('/test', (req, res) => {
-  HotelImage.find({})
-    .then(images => res.status(200).send({ images }))
-    .catch(e => res.status(400).send())
-})
 
 router.post(
   '/hotelList',
@@ -51,6 +44,38 @@ router.post(
                   localField: '_id',
                   foreignField: 'hotelId',
                   as: 'images'
+                }
+              },
+              {
+                $lookup: {
+                  from: 'hotelDescriptions',
+                  localField: '_id',
+                  foreignField: 'hotelId',
+                  as: 'description'
+                }
+              },
+              {
+                $lookup: {
+                  from: 'hotelAmenities',
+                  localField: '_id',
+                  foreignField: 'hotelId',
+                  as: 'amenities'
+                }
+              },
+              {
+                $lookup: {
+                  from: 'hotelPolicies',
+                  localField: '_id',
+                  foreignField: 'hotelId',
+                  as: 'policies'
+                }
+              },
+              {
+                $lookup: {
+                  from: 'hotelTransportations',
+                  localField: '_id',
+                  foreignField: 'hotelId',
+                  as: 'transportations'
                 }
               }
             ])
