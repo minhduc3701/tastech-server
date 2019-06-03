5dc06929e6888e16b39a7da6658f54bc6a27ea91
thanh4890 <thanh4890@gmail.com>
2019-06-03 18:54:06 +0700

    Remove console.log for joint test

--- routes/orders.js
+++ routes/orders.js
@@ -138,7 +138,6 @@ router.post('/cancel', async (req, res) => {
               }
             }
             let base64 = Buffer.from(JSON.stringify(data)).toString('base64')
-            console.log(base64)
             let cancelRes = await axios.get(
               `${process.env.PKFARE_URI}/voiding?param=${base64}`
             )
