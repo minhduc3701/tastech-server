9d4190d43d40685d267f857378b8941aaaaf9f32
phan2796 <phan2796@gmail.com>
2019-07-10 13:39:34 +0700

    combine data

--- src/routes/flights/sabre.js
+++ src/routes/flights/sabre.js
@@ -10,56 +10,44 @@ const IataCity = require('../../models/iataCity')
 const _ = require('lodash')
 const { currencyExchange } = require('../../middleware/currency')
 
-const makeFlightSegmentData = segment => {
-  let data = []
-  segment.map(e => {
-    data.push({
-      departure: e.DepartureAirport.LocationCode,
-      arrival: e.ArrivalAirport.LocationCode,
-      strDepartureDate: moment(e.DepartureDateTime).format('YYYY-MM-DD'),
-      strDepartureTime: moment(e.DepartureDateTime).format('HH:mm'),
-      strArrivalDate: moment(e.ArrivalDateTime).format('YYYY-MM-DD'),
-      strArrivalTime: moment(e.ArrivalDateTime).format('HH:mm'),
-      flightNum: e.FlightNumber,
-      flightTime: e.ElapsedTime,
-      airline: e.OperatingAirline.Code
-    })
-  })
-  return data
-}
 router.post('/shopping', currencyExchange, async (req, res) => {
+  let isRoundTrip = req.body.searchAirLegs.length === 2
+  let OriginDestinationInformation = [
+    {
+      DepartureDateTime: req.body.searchAirLegs[0].departureDate,
+      DestinationLocation: {
+        LocationCode: req.body.searchAirLegs[0].destination
+      },
+      OriginLocation: {
+        LocationCode: req.body.searchAirLegs[0].origin
+      },
+      TPA_Extensions: {
+        CabinPref: {
+          Cabin: req.body.cabinClass
+        }
+      }
+    }
+  ]
+  if (isRoundTrip) {
+    OriginDestinationInformation.push({
+      DepartureDateTime: req.body.searchAirLegs[1].departureDate,
+      DestinationLocation: {
+        LocationCode: req.body.searchAirLegs[1].destination
+      },
+      OriginLocation: {
+        LocationCode: req.body.searchAirLegs[1].origin
+      },
+      TPA_Extensions: {
+        CabinPref: {
+          Cabin: req.body.cabinClass
+        }
+      }
+    })
+  }
+  console.log(OriginDestinationInformation)
   let data = {
     OTA_AirLowFareSearchRQ: {
-      OriginDestinationInformation: [
-        {
-          DepartureDateTime: '2019-07-21T00:00:00',
-          DestinationLocation: {
-            LocationCode: 'LAX'
-          },
-          OriginLocation: {
-            LocationCode: 'NYC'
-          },
-          TPA_Extensions: {
-            CabinPref: {
-              Cabin: 'F'
-            }
-          }
-        },
-        {
-          DepartureDateTime: '2019-07-22T00:00:00',
-          DestinationLocation: {
-            LocationCode: 'NYC'
-          },
-          OriginLocation: {
-            LocationCode: 'LAX'
-          },
-          TPA_Extensions: {
-            CabinPref: {
-              Cabin: 'F'
-            }
-          }
-        }
-      ],
+      OriginDestinationInformation,
       POS: {
         Source: [
           {
@@ -80,7 +68,7 @@ router.post('/shopping', currencyExchange, async (req, res) => {
             PassengerTypeQuantity: [
               {
                 Code: 'ADT',
-                Quantity: 1
+                Quantity: req.body.adults
               }
             ]
           }
@@ -92,31 +80,93 @@ router.post('/shopping', currencyExchange, async (req, res) => {
   }
   try {
     let sabreRes = await apiSabre.shopping(data)
-    let list =
-      sabreRes.data.OTA_AirLowFareSearchRS.PricedItineraries.PricedItinerary
+    sabreRes = sabreRes.data.groupedItineraryResponse
+    let { itineraryGroups } = sabreRes
     let flights = []
-    list.map(f => {
-      let obj = {}
-      obj.departureSegments = makeFlightSegmentData(
-        f.AirItinerary.OriginDestinationOptions.OriginDestinationOption[0]
-          .FlightSegment
-      )
-      obj.returnSegments = []
-      if (f.AirItinerary.DirectionInd === 'Return') {
-        obj.returnSegments = makeFlightSegmentData(
-          f.AirItinerary.OriginDestinationOptions.OriginDestinationOption[1]
-            .FlightSegment
+    itineraryGroups.map(l => {
+      l.itineraries.map(i => {
+        let obj = {
+          legs: i.legs
+        }
+        obj.departureDescs = sabreRes.legDescs.find(
+          leg => leg.id === i.legs[0].ref
         )
-      }
-      obj.rawCurrency =
-        f.AirItineraryPricingInfo[0].ItinTotalFare.TotalFare.CurrencyCode
-      obj.rawTotalPrice =
-        f.AirItineraryPricingInfo[0].ItinTotalFare.TotalFare.Amount
-      obj.supplier = 'sabre'
-      ;(obj.currency = req.currency.code),
-        (obj.totalPrice = obj.rawTotalPrice * req.currency.rate)
-      obj.price = obj.rawTotalPrice * req.currency.rate
-      flights.push(obj)
+        obj.departureSegments = []
+        obj.departureDescs.schedules.map(s => {
+          let data = sabreRes.scheduleDescs.find(sch => sch.id === s.ref)
+          obj.departureSegments.push({
+            id: data.id,
+            departure: data.departure.airport,
+            arrival: data.arrival.airport,
+            strDepartureTime: data.departure.time.substring(0, 5),
+            strArrivalTime: data.arrival.time.substring(0, 5),
+            flightNum: data.carrier.marketingFlightNumber,
+            flightTime: moment(data.arrival.time.substring(0, 5), 'hh:mm').diff(
+              moment(data.departure.time.substring(0, 5), 'hh:mm'),
+              'minutes'
+            ),
+            airline: data.carrier.marketing
+          })
+        })
+        obj.departureFlight = {
+          flightId: ''
+        }
+        obj.departureSegments.forEach(s => {
+          if (obj.departureFlight.flightId === '') {
+            obj.departureFlight.flightId += `${s.flightNum}-${s.airline}`
+          } else {
+            obj.departureFlight.flightId += `-${s.flightNum}-${s.airline}`
+          }
+        })
+
+        obj.returnDescs = {}
+        obj.returnSegments = []
+        obj.returnFlight = {}
+        if (i.legs.length === 2) {
+          obj.returnDescs = sabreRes.legDescs.find(
+            leg => leg.id === i.legs[1].ref
+          )
+          obj.returnSegments = []
+          obj.returnDescs.schedules.map(s => {
+            let data = sabreRes.scheduleDescs.find(sch => sch.id === s.ref)
+            obj.returnSegments.push({
+              id: data.id,
+              departure: data.departure.airport,
+              arrival: data.arrival.airport,
+              strDepartureTime: data.departure.time.substring(0, 5),
+              strArrivalTime: data.arrival.time.substring(0, 5),
+              flightNum: data.carrier.marketingFlightNumber,
+              flightTime: moment(
+                data.arrival.time.substring(0, 5),
+                'hh:mm'
+              ).diff(
+                moment(data.departure.time.substring(0, 5), 'hh:mm'),
+                'minutes'
+              ),
+              airline: data.carrier.marketing
+            })
+          })
+          obj.returnFlight = {
+            flightId: ''
+          }
+          obj.returnSegments.forEach(s => {
+            if (obj.returnFlight.flightId === '') {
+              obj.returnFlight.flightId += `${s.flightNum}-${s.airline}`
+            } else {
+              obj.returnFlight.flightId += `-${s.flightNum}-${s.airline}`
+            }
+          })
+        }
+
+        obj.rawCurrency = i.pricingInformation[0].fare.totalFare.currency
+        obj.rawTotalPrice = i.pricingInformation[0].fare.totalFare.totalPrice
+        obj.currency = req.currency.code
+        obj.totalPrice = obj.rawTotalPrice * req.currency.rate
+        obj.price = obj.rawTotalPrice * req.currency.rate
+        obj.supplier = 'sabre'
+
+        flights.push(obj)
+      })
     })
     let airlines = []
     let airports = []
