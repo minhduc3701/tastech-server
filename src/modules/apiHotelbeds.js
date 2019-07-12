1a68716d11962a117c4342b5886ceb5ed084fbaa
Giang Do <ngocgiang2512@gmail.com>
2019-07-12 17:00:32 +0700

    refactor generate header

--- src/modules/apiHotelbeds.js
+++ src/modules/apiHotelbeds.js
@@ -2,8 +2,7 @@ const axios = require('axios')
 const { generateHeader } = require('../config/hotelbeds')
 
 const hotelbedsHttp = axios.create({
-  baseURL: process.env.HOTELBEDS_URI,
-  headers: generateHeader()
+  baseURL: process.env.HOTELBEDS_URI
 })
 
 const endpoints = {
@@ -13,15 +12,20 @@ const endpoints = {
 
 const api = {
   getHotels: queryString => {
-    return hotelbedsHttp.get(`${endpoints.hotelContents}?${queryString}`)
+    return hotelbedsHttp.get(`${endpoints.hotelContents}?${queryString}`, {
+      headers: generateHeader()
+    })
   },
   getHotelDetail: hotelCode => {
     return hotelbedsHttp.get(
-      `${endpoints.hotelContents}/${hotelCode}/details?language=ENG`
+      `${endpoints.hotelContents}/${hotelCode}/details?language=ENG`,
+      { headers: generateHeader() }
     )
   },
   getRooms: request => {
-    return hotelbedsHttp.post(`${endpoints.hotels}`, request)
+    return hotelbedsHttp.post(`${endpoints.hotels}`, request, {
+      headers: generateHeader()
+    })
   }
 }
 
