51c9d86c1daf51e000e647614ce4a95b49734548
thanh4890 <thanh4890@gmail.com>
2019-09-03 13:53:36 +0700

    Update expense email subjects

--- src/mailTemplates/changeExpenseStatus.js
+++ src/mailTemplates/changeExpenseStatus.js
@@ -5,7 +5,7 @@ function changeExpenseStatus(user, expense) {
       return {
         to: user.email,
         from: `EzBizTrip <${process.env.EMAIL_NO_REPLY}>`,
-        subject: `Your trip request has been approved`,
+        subject: `Your expense has been reimbursed`,
         html: `Congratulation,  ${user.firstName}! \n\n
           A reimbursement payment for your expense has been marked as Paid by your accountant. Detail:<br/>
           Trip: ${expense._trip.name}<br/>
@@ -18,7 +18,7 @@ function changeExpenseStatus(user, expense) {
       return {
         to: user.email,
         from: `EzBizTrip <${process.env.EMAIL_NO_REPLY}>`,
-        subject: `Your trip request has been rejected`,
+        subject: `Your expense claim has been rejected`,
         html: `Dear ${user.firstName}! <br/>
           Your following expense claim was rejected:
           Date incurred: ${moment().format('lll')}<br/>
