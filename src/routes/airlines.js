7599f564193f0149f35c66b456509f883cf9dbf2
thanh4890 <thanh4890@gmail.com>
2019-10-14 15:55:06 +0700

    Add airlines search resources and also save prefer airline name

---
+++ src/routes/airlines.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+const Airline = require('../models/airline')
+const _ = require('lodash')
+
+router.get('/search', function(req, res, next) {
+  let keyword = _.trim(_.toLower(req.query.keyword))
+
+  Airline.find({
+    iata: {
+      $nin: ['', '-', 'IATA'] // exclude dummy data
+    },
+    $or: [
+      {
+        name: {
+          $regex: new RegExp(keyword),
+          $options: 'i'
+        }
+      },
+      {
+        iata: {
+          $regex: new RegExp(keyword),
+          $options: 'i'
+        }
+      }
+    ]
+  })
+    .limit(10)
+    .then(airlines => {
+      res.status(200).send({ airlines })
+    })
+    .catch(e => {
+      res.status(400).send()
+    })
+})
+
+module.exports = router
