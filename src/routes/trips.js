b9fd2d41facfa339a506be9b92073d613e63e5f4
phan2796 <phan2796@gmail.com>
2019-11-15 17:55:57 +0700

    add class flight in create trip

--- src/routes/trips.js
+++ src/routes/trips.js
@@ -454,15 +454,18 @@ router.post(
       }
 
       // calculate Flight budget
+
       if (trip.budgetPassengers[0].flight.selected) {
         let budgetRequest = makeSabreSearchRequestFromBudget(
           trip.budgetPassengers[0].flight,
           policy
         )
+        trip.budgetPassengers[0].flight.class = policy.flightClass.toLowerCase()
         let sabreRes = await apiSabre.shopping(
           makeSabreRequestData(budgetRequest),
           req.sabreRestToken
         )
+        trip.budgetPassengers[0].flight.class
         sabreRes = sabreRes.data.groupedItineraryResponse
 
         let flights = makeSabreFlightsData(sabreRes, req.currency, 1)
