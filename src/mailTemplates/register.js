f80649020ee4005d11459f7920544cad2e18b352
Giang Do <ngocgiang2512@gmail.com>
2019-11-26 16:17:14 +0700

    refactor

--- src/mailTemplates/register.js
+++ src/mailTemplates/register.js
@@ -2,22 +2,23 @@ const { renderMail } = require('../config/mail')
 
 async function register(user, token, userCreator) {
   let html
+  let bcc
   if (
     userCreator &&
     user._role.type === 'admin' &&
     userCreator._role.type === 'tas-admin'
   ) {
-    html = await renderMail('register-admin', {
-      title: 'Welcome to EzBizTrip',
-      bcc: [process.env.EMAIL_SALES],
-      link: `${process.env.APP_URI}/reset-password/${token}`,
-      user,
-      loginLink: `${process.env.APP_URI}/login`,
-      note:
-        process.env.NODE_ENV === 'production'
-          ? 'We have created account for you:'
-          : 'We have created 30 days demo account for you:'
-    })
+    ;(bcc = [process.env.EMAIL_SALES]),
+      (html = await renderMail('register-admin', {
+        title: 'Welcome to EzBizTrip',
+        link: `${process.env.APP_URI}/reset-password/${token}`,
+        user,
+        loginLink: `${process.env.APP_URI}/login`,
+        note:
+          process.env.NODE_ENV === 'production'
+            ? 'We have created account for you:'
+            : 'We have created 30 days demo account for you:'
+      }))
   } else {
     html = await renderMail('register', {
       title: 'Welcome to EzBizTrip',
@@ -27,6 +28,7 @@ async function register(user, token, userCreator) {
   }
   return {
     to: user.email,
+    bcc,
     from: `${process.env.EMAIL_CONTACT_ALIAS} <${process.env.EMAIL_NO_REPLY}>`,
     subject: `Welcome to EzBizTrip`,
     html
