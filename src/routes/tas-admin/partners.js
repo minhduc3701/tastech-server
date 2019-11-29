a8043c666fe8b80453c2104148e2954187f20ed6
phan2796 <phan2796@gmail.com>
2019-11-29 11:57:37 +0700

    create, edit partner

--- src/routes/tas-admin/partners.js
+++ src/routes/tas-admin/partners.js
@@ -43,123 +43,44 @@ router.get('/:id', function(req, res) {
     })
 })
 
-// router.post('/search', (req, res) => {
-//   Company.find({
-//     name: new RegExp(req.body.s, 'i')
-//   })
-//     .limit(10)
-//     .then(companies => {
-//       res.status(200).send({ companies })
-//     })
-//     .catch(e => {
-//       res.status(400).send()
-//     })
-// })
+router.post('/', function(req, res) {
+  let partner = new Partner(req.body)
+  partner
+    .save()
+    .then(partner => {
+      let role = new Role({
+        name: 'Partner',
+        type: 'partner',
+        _partner: partner._id
+      })
+      return role.save()
+    })
+    .then(role => res.status(200).send({ partner, role }))
+    .catch(e => {
+      res.status(400).send(e)
+    })
+})
 
-// router.post('/', function(req, res) {
-//   let body = _.pick(req.body, ['name', 'currency'])
+router.patch('/:id', function(req, res) {
+  let id = req.params.id
 
-//   let company = new Company({
-//     _creator: req.user._id,
-//     ...body
-//   })
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
 
-//   company
-//     .save()
-//     .then(company => {
-//       return Role.insertMany(
-//         roles.map(role => ({
-//           ...role,
-//           _company: company._id
-//         }))
-//       )
-//     })
-//     .then(roles => {
-//       return api.currency(company.currency)
-//     })
-//     .then(currency => {
-//       let rate = currency.data[0].rate
-//       let policy = new Policy({
-//         name: 'Default Policy',
-//         _company: company._id,
-//         status: 'default',
-//         flightClass: 'Economy',
-//         stops: '0',
-//         setDaysBeforeFlights: false,
-//         daysBeforeFlights: 7,
-//         setFlightLimit: false,
-//         flightLimit: 500 * rate,
-//         flightNotification: 'no',
-//         flightApproval: 'no',
-//         hotelClass: 3,
-//         hotelSearchDistance: 15,
-//         setDaysBeforeLodging: false,
-//         daysBeforeLodging: 7,
-//         setHotelLimit: false,
-//         hotelLimit: 500 * rate,
-//         hotelNotification: 'no',
-//         hotelApproval: 'no',
-//         setTransportLimit: true,
-//         transportLimit: 10 * rate,
-//         setMealLimit: true,
-//         mealLimit: 10 * rate,
-//         setProvision: true,
-//         provision: 5
-//       })
+  let body = req.body
 
-//       return policy.save()
-//     })
-//     .then(policy => {
-//       company._policy = policy._id
-//       return company.save()
-//     })
-//     .then(company => res.status(200).send({ company }))
-//     .catch(e => {
-//       res.status(400).send()
-//     })
-// })
-
-// router.get('/:id', function(req, res) {
-//   let id = req.params.id
-
-//   if (!ObjectID.isValid(id)) {
-//     return res.status(404).send()
-//   }
-
-//   Company.findById(id)
-//     .then(company => {
-//       if (!company) {
-//         return res.status(404).send()
-//       }
-
-//       res.status(200).send({ company })
-//     })
-//     .catch(e => {
-//       res.status(400).send()
-//     })
-// })
-
-// router.patch('/:id', function(req, res) {
-//   let id = req.params.id
-
-//   if (!ObjectID.isValid(id)) {
-//     return res.status(404).send()
-//   }
-
-//   let body = _.pick(req.body, ['name', 'currency'])
-
-//   Company.findByIdAndUpdate(id, { $set: body }, { new: true })
-//     .then(company => {
-//       if (!company) {
-//         return res.status(404).send()
-//       }
-
-//       res.status(200).send({ company })
-//     })
-//     .catch(e => {
-//       res.status(400).send()
-//     })
-// })
+  Partner.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(partner => {
+      if (!partner) {
+        return res.status(404).send()
+      }
+      res.status(200).send({ partner })
+    })
+    .catch(e => {
+      res.status(400).send()
+    })
+})
 
 // router.delete('/:id', function(req, res) {
 //   let id = req.params.id
