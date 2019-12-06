27f9aa01c05a4621a5fa33b17451aa466b768312
phan2796 <phan2796@gmail.com>
2019-12-06 17:48:31 +0700

    partner edit order

--- src/routes/partner-admin/orders.js
+++ src/routes/partner-admin/orders.js
@@ -3,6 +3,11 @@ const router = express.Router()
 const Order = require('../../models/order')
 const { ObjectID } = require('mongodb')
 const _ = require('lodash')
+const { emailEmployeeItinerary } = require('../../middleware/email')
+const {
+  refundCancelledOrderManually,
+  emailCustomerCancelledOrder
+} = require('../../middleware/orders')
 
 router.get('/', function(req, res, next) {
   let perPage = _.get(req.query, 'perPage', 50)
@@ -67,5 +72,56 @@ router.get('/:id', function(req, res, next) {
       res.status(400).send()
     })
 })
+
+router.patch(
+  '/:id',
+  async (req, res, next) => {
+    let id = req.params.id
+
+    if (!ObjectID.isValid(id)) {
+      return res.status(404).send()
+    }
+
+    const body = _.pick(req.body, ['status', 'pnr'])
+
+    try {
+      let order = await Order.findOneAndUpdate(
+        {
+          _id: id,
+          _partner: req.user._partner,
+          status: { $ne: 'cancelled' } // don't update cancelled order
+        },
+        { $set: body },
+        { new: true }
+      )
+
+      if (!order) {
+        return res.status(404).send()
+      }
+
+      res.status(200).send({ order })
+
+      if (order.status === 'completed') {
+        //for sending email to employee
+        req.trip = {
+          _id: order._trip
+        }
+        return next()
+      }
+
+      // refund order manually
+      if (order.status === 'cancelled') {
+        req.cancelledOrder = order
+        req.cancelCharge = req.body.cancelCharge
+        return next()
+      }
+    } catch (e) {
+      res.status(400).send()
+    }
+  },
+  refundCancelledOrderManually,
+  emailCustomerCancelledOrder,
+  emailEmployeeItinerary
+)
 
 module.exports = router
