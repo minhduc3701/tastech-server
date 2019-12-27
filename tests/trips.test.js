75ab24d595f473d098c66f03d6b26ba36ba207e5
phan2796 <phan2796@gmail.com>
2019-12-27 16:25:54 +0700

    add test

---
+++ tests/trips.test.js
@@ -0,0 +1,34 @@
+const request = require('supertest')
+const app = require('../src/app')
+const mongoose = require('mongoose')
+
+const {
+  userToken,
+  tripApprovedId,
+  setupDatabase,
+  user2Token
+} = require('./fixtures/db.js')
+
+beforeEach(setupDatabase)
+
+// get expenses by trip id
+test('Should get expense with valid tripId', async () => {
+  await request(app)
+    .get(`/trips/${tripApprovedId}/expenses`)
+    .set('Authorization', `Bearer ${userToken}`)
+    .expect(200)
+})
+
+test('Should not get expense with invalid tripId', async () => {
+  await request(app)
+    .get(`/trips/123123123/expenses`)
+    .set('Authorization', `Bearer ${userToken}`)
+    .expect(404)
+})
+
+test('Should not get expense of trip of other users', async () => {
+  await request(app)
+    .get(`/trips/${tripApprovedId}/expenses`)
+    .set('Authorization', `Bearer ${user2Token}`)
+    .expect(404)
+})
