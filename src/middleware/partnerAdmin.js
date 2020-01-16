3cb36fb8b5065bd7df1a1c24057ce3119f21847c
thanh4890 <thanh4890@gmail.com>
2020-01-16 16:52:18 +0700

    Refactor condition to book on behalf

--- src/middleware/partnerAdmin.js
+++ src/middleware/partnerAdmin.js
@@ -2,17 +2,32 @@ const Role = require('../models/role')
 const User = require('../models/user')
 
 const isPartnerBooking = async (req, res, next) => {
+  if (!req.body.onBehalf) {
+    return next()
+  }
+
   try {
     let role = await Role.findById(req.user._role)
     if (role.type === 'partner-admin') {
-      req.partnerAdmin = req.user
+      let user = await User.findOne({
+        _id: req.body.onBehalf,
+        _partner: req.user._partner
+      })
 
-      if (req.body.onBehalf) {
-        let user = await User.findById(req.body.onBehalf)
-        req.user = user
+      // not found user
+      if (!user) {
+        return res.status(400).send({
+          message: 'Cannot book on behalf'
+        })
       }
+
+      // swap onbehalf employee to req.user
+      // and partner admin to req.partnerAdmin
+      req.partnerAdmin = req.user
+      req.user = user
     }
   } catch (error) {}
+
   next()
 }
 
