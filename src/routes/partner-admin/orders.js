99591c2e731451e4a1f2d872812604109099ff9a
phan2796 <phan2796@gmail.com>
2020-02-14 18:22:57 +0700

    filter orders

--- src/routes/partner-admin/orders.js
+++ src/routes/partner-admin/orders.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Order = require('../../models/order')
-const Trip = require('../../models/trip')
+const User = require('../../models/user')
 const { ObjectID } = require('mongodb')
 const _ = require('lodash')
 const { emailEmployeeItinerary } = require('../../middleware/email')
@@ -183,7 +183,6 @@ router.patch(
         return next()
       }
     } catch (e) {
-      console.log(e)
       res.status(400).send()
     }
   },
@@ -192,5 +191,108 @@ router.patch(
   emailCustomerCancelledOrder,
   emailEmployeeItinerary
 )
+
+router.get('/:id/logs', (req, res) => {
+  if (!ObjectID.isValid(req.params.id)) {
+    return res.status(404).send()
+  }
+  console.log(1)
+  const perPage = Number(_.get(req, 'query.perPage', 10))
+  const page = Number(_.get(req, 'query.page', 0))
+  try {
+    Promise.all([
+      User.find({
+        email: new RegExp(_.get(req, 'query.s', ''), 'i')
+      })
+    ])
+      .then(results => {
+        const userIds = results[0].map(user => user._id)
+        return Promise.all([
+          Order.aggregate([
+            {
+              $match: {
+                _id: new ObjectID(req.params.id),
+                _partner: req.user._partner
+              }
+            },
+            { $unwind: '$logs' },
+            {
+              $group: {
+                _id: '$logs._id',
+                _creator: { $first: '$logs._creator' },
+                createdAt: { $first: '$logs.createdAt' },
+                // field: { $first: '$logs.field' },
+                // old: { $first: '$logs.old' },
+                // new: { $first: '$logs.new' },
+                changedValues: { $first: '$logs.changedValues' },
+                note: { $first: '$logs.note' }
+              }
+            },
+            {
+              $match: {
+                _creator: { $in: userIds }
+              }
+            },
+            {
+              $sort: { createdAt: -1 }
+            },
+            { $skip: perPage * page },
+            { $limit: perPage }
+          ]),
+          Order.aggregate([
+            {
+              $match: {
+                _id: new ObjectID(req.params.id),
+                _partner: req.user._partner
+              }
+            },
+            { $unwind: '$logs' },
+            {
+              $group: {
+                _id: '$logs._id',
+                _creator: { $first: '$logs._creator' }
+              }
+            },
+            {
+              $match: {
+                _creator: { $in: userIds }
+              }
+            }
+          ])
+        ])
+      })
+      .then(results => {
+        console.log('results  0: ', results[0])
+        console.log('results  1: ', results[0].length)
+        return Promise.all([
+          User.populate(results[0], [
+            {
+              path: '_creator',
+              select: 'email username firstName lastName avatar'
+            }
+          ]),
+          results[1].length
+        ])
+      })
+      .then(results => {
+        let logs = results[0]
+        let total = results[1]
+        res.status(200).send({
+          logs,
+          totalPage: Math.ceil(total / perPage),
+          total,
+          count: logs.length,
+          perPage,
+          page
+        })
+      })
+      .catch(e => {
+        console.log(e)
+        res.status(400).send()
+      })
+  } catch (error) {
+    console.log(e)
+  }
+})
 
 module.exports = router
