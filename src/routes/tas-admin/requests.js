442c14db55285bc35fb104e3637cfdab8442e389
thanh4890 <thanh4890@gmail.com>
2020-02-24 18:29:54 +0700

    Pagination for tas admin requests

--- src/routes/tas-admin/requests.js
+++ src/routes/tas-admin/requests.js
@@ -5,38 +5,42 @@ const User = require('../../models/user')
 const _ = require('lodash')
 const { ObjectID } = require('mongodb')
 
-router.get('/', function(req, res) {
-  Request.aggregate([
-    {
-      $lookup: {
-        from: 'users',
-        localField: 'email',
-        foreignField: 'email',
-        as: 'users'
-      }
-    },
-    {
-      $project: {
-        _id: 1,
-        status: 1,
-        email: 1,
-        notes: 1,
-        'users.disabled': 1,
-        'users.email': 1,
-        'users._id': 1,
-        referral: 1
-      }
-    },
-    {
-      $sort: {
-        _id: -1
-      }
-    }
-  ])
-    .then(requests => {
-      res.status(200).send({ requests })
+router.get('/', async function(req, res) {
+  let perPage = 10
+  let page = Math.max(0, _.get(req, 'query.page', 0))
+
+  try {
+    let results = await Promise.all([
+      Request.find()
+        .limit(perPage)
+        .skip(perPage * page)
+        .sort([['_id', -1]]),
+      Request.countDocuments()
+    ])
+
+    let requests = results[0]
+    let total = results[1]
+    let users = await User.find({
+      email: { $in: requests.map(r => r.email) }
     })
-    .catch(e => res.status(400).send())
+
+    requests = requests.map(r => ({
+      ...r.toJSON(),
+      users: users
+        .filter(user => user.email === r.email)
+        .map(user => _.pick(user, ['disabled', 'email', '_id']))
+    }))
+
+    res.status(200).send({
+      requests,
+      page,
+      perPage,
+      total,
+      totalPage: Math.ceil(total / perPage)
+    })
+  } catch (e) {
+    res.status(400).send()
+  }
 })
 
 router.get('/:id', function(req, res) {
